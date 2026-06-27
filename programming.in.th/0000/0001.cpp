80798f0bed8376d9c77d8f36979491b9e7ff4878
Lewyns <lewyns1984@gmail.com>
2026-06-27 14:12:37 +0700

    Add programming.in.th task 0001

---
+++ programming.in.th/0000/0001.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c, x;
+    cout << "";
+    cin >> a >> b >> c;
+    x = a + b + c;
+
+    if (x >= 80 && x <= 100){
+        cout << "A";
+    }
+    else if (x >= 75 && x <=79){
+        cout << "B+";
+    }
+    else if (x >= 70 && x <= 74){
+        cout << "B";
+    }
+    else if (x >= 65 && x <= 69){
+        cout << "C+";
+    }
+    else if (x >= 60 && x <= 64){
+        cout << "C";
+    }
+    else if (x >= 55 && x <= 59){
+        cout << "D+";
+    }
+    else if (x >= 50 && x <= 54){
+        cout << "D";
+    }
+    else{
+        cout << "F";
+    }
+    return 0;
+}
